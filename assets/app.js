451b1bbf722f4ce939afeb2a175fb813dc6e8e1a
sofiacrf <sofia.rojasflo@gmail.com>
2022-03-07 17:30:14 -0600

    added store products onject

---
+++ assets/app.js
@@ -0,0 +1,17 @@
+// store products in object
+const products = [
+    {
+        title: 'Picture 1',
+        imageUrl: 'https://unsplash.com/photos/Z8TQv3yKQd4',
+        price: 500.00,
+        description: 'flores'
+    }, 
+    {
+        title: 'Picture 2',
+        imageUrl: 'https://unsplash.com/photos/Nyvq2juw4_o',
+        price: 550.00,
+        description: 'city picture'
+    }
+]; 
+
+// Add a product list object
